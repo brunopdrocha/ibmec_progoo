5f306a3e72c6689a95eb1532bccbf389bc354807
brunopdrocha <brunopdrocha@gmail.com>
2023-09-01 09:29:06 -0300

    Aula relatando o tipo de classes

---
+++ aula04/App.java
@@ -0,0 +1,17 @@
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Pessoa p1;
+        Jogador j1 = new Jogador("Socha", "c2", "VASCO", "Unif1", 4);
+
+        System.out.println(j1.nome);
+        j1.movimenta(10, -5);
+        System.out.println(j1.posX + " , " + j1.posY);
+        
+        //downCasting
+        p1 = new Jogador("Carlos", "c3", "IBMEC", "unif1", 01);
+        j1.falar();
+        j1.movimenta(10, 0); // Corrigido a ordem de chamada dos métodos
+
+    }
+}
